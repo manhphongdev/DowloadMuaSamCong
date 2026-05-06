1a3ee2c8fa9c07498283e3b87002a4f0a4740bf6
manhphong <tranmanhphong2002@gmail.com>
2026-05-06 08:19:20 +0700

    chore(release): v1.0.10

--- src/main/java/vn/muasamcong/downloader/app/AppInfo.java
+++ src/main/java/vn/muasamcong/downloader/app/AppInfo.java
@@ -3,7 +3,7 @@ package vn.muasamcong.downloader.app;
 public final class AppInfo {
 
     public static final String APP_NAME = "Muasamcong.mpi.gov.vn Downloader";
-    public static final String VERSION = "1.0.9";
+    public static final String VERSION = "1.0.10";
 
     private AppInfo() {
     }
